23b0129cb123192eeb828a88283a193f320c606a
Lovnyth Valcin <lovnyth@gmail.com>
2025-03-11 01:08:34 +0000

    filte function added

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -2,9 +2,12 @@ import React, {useState} from "react";
 
 // const listArray = ["Grocery shopping", "Do Laundry", "Meal Prep","Finish Coding Projects"]
 
+
 //create your first component
 const Home = () => {
 	const [listArray, setListArray]=useState(["Grocery shopping", "Do Laundry", "Meal Prep","Finish Coding Projects"])
+	const result = listArray.filter((item)=> item !="Meal Prep")
+	console.log(result);
 	return (
 		<div className="text-center">
 			<h1>To-Do List</h1>
@@ -17,11 +20,17 @@ const Home = () => {
 				})
 			}
 			</ul>
-			<input type="text" onChange={
-				(e)=>
-				{console.log(e.target.value)
+			<input 
+				type="text"
+				onKeyDown={
+				(e)=>{
+					if(e.key==='Enter'){
+						const newListArray = [...listArray,e.target.value]
+						setListArray(newListArray)
+					}		
 				}
-				}/>
+				}
+			/>
 			<button>Click me</button>
 
 		</div>
