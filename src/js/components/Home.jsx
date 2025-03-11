d8cb02c74eba015afeccbedd12d3e0862e36534e
Lovnyth Valcin <lovnyth@gmail.com>
2025-03-11 00:21:51 +0000

    capturing input value with Onchange

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,20 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 
-const todoList = ["Grocery shopping", "Do Laundry", "Meal Prep","Finish Coding Projects"]
+// const listArray = ["Grocery shopping", "Do Laundry", "Meal Prep","Finish Coding Projects"]
 
 //create your first component
 const Home = () => {
+	const [listArray, setListArray]=useState(["Grocery shopping", "Do Laundry", "Meal Prep","Finish Coding Projects"])
 	return (
 		<div className="text-center">
-			<input type="text"/><button>Click me</button>
-            {todoList.map(
+			<h1>To-Do List</h1>
+			<ul>
+            {listArray.map(
 				(item)=>{
 					return (<div>
 						<li>{item}</li>
 					</div>)
-				}
-			)
+				})
 			}
+			</ul>
+			<input type="text" onChange={
+				(e)=>
+				{console.log(e.target.value)
+				}
+				}/>
+			<button>Click me</button>
 
 		</div>
 	);
